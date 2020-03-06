fe937e8f31c595e4fde299346b079efd2a3fe9c3
Gabe Dunn <gabe.dunn@shaw.ca>
2020-03-05 23:39:11 -0800

    add basis of moderation submodule

--- src/Devmod.ts
+++ src/Devmod.ts
@@ -13,6 +13,7 @@ import { hydrateChannels } from './utils/config/hydrateChannels'
 import { hydrateRoles } from './utils/config/hydrateRoles'
 import { handleErrors } from './utils/handleErrors'
 import { log } from './utils/log'
+import { Moderation } from './utils/functions/Moderation'
 
 // The main bot class
 export class Devmod {
@@ -20,6 +21,9 @@ export class Devmod {
     // The client, config, and processes are all accessible from anywhere within the class
     private readonly client: Client
     private readonly config: ConfigInterface
+
+    // Declare the submodules
+    public readonly moderation: Moderation
 
     // The bot will be connected once the constructor is called
     constructor (processes: ProcessInterface[], config: UserConfigInterface) {
@@ -31,10 +35,14 @@ export class Devmod {
             // Once the bot has logged in, hydrate the config and log a successful login
             this.hydrateConfig()
             log('Constructor', `Logged in as ${this.client.user.tag}.`)
+            this.moderation.sayHi()
         })
 
         // Log the bot it
         this.client.login(this.config.token).then()
+
+        // Initialize and assign the submodules
+        this.moderation = new Moderation(this.client, this.config)
 
         // Initialize all the processes.
         for (const process of processes) {
---
+++ src/utils/functions/Moderation.ts
@@ -0,0 +1,23 @@
+/*
+ * Gabe Dunn 2020
+ * Class file for the bot's moderation functions
+ */
+
+import { Client } from "discord.js"
+import { ConfigInterface } from '../../types/interfaces/ConfigInterface'
+import { log } from '../log'
+
+export class Moderation {
+    // The client, config, and processes are all accessible from anywhere within the class
+    private readonly client: Client
+    private readonly config: ConfigInterface
+
+    constructor (client: Client, config: ConfigInterface) {
+        this.client = client
+        this.config = config
+    }
+
+    public sayHi() {
+        log('Say Hi', this.config.repEmote)
+    }
+}
