c13ef98bca52eee1c4c17ffbcea3e3e2053dfb4a
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-03-29 19:38:39 -0700

    Set all default configuration to blank

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@
 
 import { Devmod } from './Devmod'
 import { config } from 'dotenv'
-import { Client, DMChannel, GuildMember, Message, PartialMessage, TextChannel } from 'discord.js'
+import {
+    Client,
+    DMChannel,
+    GuildMember,
+    Message,
+    PartialMessage,
+    TextChannel
+} from 'discord.js'
 import { ConfigInterface } from './types/interfaces/ConfigInterface'
 import { log } from './utils/log'
 import { CommandInterface } from './types/interfaces/CommandInterface'
@@ -16,18 +23,18 @@ config()
 const main = () => {
     const config = {
         token: process.env.BOT_TOKEN || '',
-        guildID: '431641323578327050',
+        guildID: '',
         channelIDs: {
-            warn: '432010493213802517',
-            ban: '432010505000058881',
-            reports: '432998514193334283',
-            assignRoles: '593131994096074764',
-            info: '598697397039792128',
-            errors: '618740166630309909'
+            warn: '',
+            ban: '',
+            reports: '',
+            assignRoles: '',
+            info: '',
+            errors: ''
         },
         roleIDs: {
-            muted: '593132538285916171',
-            verified: '598708805487820811'
+            muted: '',
+            verified: ''
         }
     }
     const commands: CommandInterface[] = [
@@ -37,9 +44,20 @@ const main = () => {
             category: 'utils',
             description: 'Test command',
             permissions: ['ADMINISTRATOR'],
-            exec (message: Message | PartialMessage, args: string[], channel: TextChannel | DMChannel, member: GuildMember, client: Client, config: ConfigInterface, sub: SubmodulesInterface) {
+            exec(
+                message: Message | PartialMessage,
+                args: string[],
+                channel: TextChannel | DMChannel,
+                member: GuildMember,
+                client: Client,
+                config: ConfigInterface,
+                sub: SubmodulesInterface
+            ) {
                 log('Test', `Test Message: ${args.join(' ') || 'test!'}`)
-                return channel.send(`Test Message: ${args.join(' ') || 'test!'}`).then().catch()
+                return channel
+                    .send(`Test Message: ${args.join(' ') || 'test!'}`)
+                    .then()
+                    .catch()
             }
         }
     ]
