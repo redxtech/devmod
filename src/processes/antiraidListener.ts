38234798921bfc4628bc7ff727ea335e2ded061c
Austin M <austin@austinmccalley.com>
2020-04-20 13:33:26 -0700

    :bug: Spam in DM causes errors
    Spam in DM's causes errors and could crash bot

--- src/processes/antiraidListener.ts
+++ src/processes/antiraidListener.ts
@@ -30,7 +30,10 @@ export const antiraidListener: ProcessInterface = {
         // console.log(staffRole, seniorRole)
 
         client.on('message', async (message: Message) => {
-            if (message.author.bot) return
+            if (message.author.bot || message.channel.type === 'dm') return
+            console.log(message.channel.type)
+            console.log('HERE')
+
             const lastKnownTimestamp = mentions.get(message.author.id) ?? null
 
             const userRoleIds = message.member.roles.cache.map(r => r.id)
