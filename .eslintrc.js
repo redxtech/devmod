43b42a91bd7b05d06e0ab65a9c5d4323b7448c98
Gabe Dunn <gabed@hey.com>
2020-08-21 16:47:53 -0600

    add more eslint plugins and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,17 +10,27 @@ module.exports = {
 		project: './tsconfig.json',
 		warnOnUnsupportedTypeScriptVersion: false
 	},
-	plugins: ['@typescript-eslint'],
+	plugins: [
+		'@typescript-eslint',
+		'eslint-comments',
+		'no-use-extend-native',
+		'unicorn',
+		'standard'
+	],
 	extends: [
 		'eslint:recommended',
 		'plugin:import/errors',
 		'plugin:import/warnings',
 		'plugin:node/recommended',
 		'plugin:promise/recommended',
+		'plugin:eslint-comments/recommended',
+		'plugin:no-use-extend-native/recommended',
+		'plugin:unicorn/recommended',
 		'standard-with-typescript',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'prettier',
+		'prettier/unicorn',
 		'prettier/standard',
 		'prettier/@typescript-eslint'
 	]
