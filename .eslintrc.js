a07f2edf761dc25b341716f51c595b95f66bcaea
Gabe Dunn <gabed@hey.com>
2020-08-21 17:49:58 -0600

    add more configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,32 @@ module.exports = {
 	},
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
+		extraFileExtensions: ['.json'],
 		tsconfigRootDir: __dirname,
 		project: './tsconfig.json',
 		warnOnUnsupportedTypeScriptVersion: false
 	},
+	rules: {
+		'sort-imports': 'off',
+		'import/order': 'off',
+		'simple-import-sort/sort': 'error',
+		'optimize-regex/optimize-regex': 'warn',
+		'no-secrets/no-secrets': 'error'
+	},
+	settings: {
+		'json/json-with-comments-files': []
+	},
 	plugins: [
 		'@typescript-eslint',
+		'json-format',
 		'eslint-comments',
 		'no-use-extend-native',
+		'sonarjs',
+		'optimize-regex',
+		'simple-import-sort',
+		'perf-standard',
+		'no-secrets',
+		'security',
 		'unicorn',
 		'standard'
 	],
@@ -25,6 +43,9 @@ module.exports = {
 		'plugin:promise/recommended',
 		'plugin:eslint-comments/recommended',
 		'plugin:no-use-extend-native/recommended',
+		'plugin:sonarjs/recommended',
+		'perf-standard',
+		'plugin:security/recommended',
 		'plugin:unicorn/recommended',
 		'standard-with-typescript',
 		'plugin:@typescript-eslint/recommended',
