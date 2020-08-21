c0c5e816a5901b0fa682886aca2e0f6561e548b5
Gabe Dunn <gabed@hey.com>
2020-08-21 16:31:40 -0600

    add prettier and standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,11 @@ module.exports = {
 	},
 	plugins: ['@typescript-eslint'],
 	extends: [
-		'eslint:recommended',
+		'standard-with-typescript',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking'
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+		'prettier/standard',
+		'prettier/@typescript-eslint'
 	]
 }
